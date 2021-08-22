d35f614b49416da41151068135035cb6f180cb9a
rojajullapalli <rojajules@gmail.com>
2021-08-22 17:09:20 +0530

    ADDED:Three Float Values To Find Maximum

--- src/com/bridgelabz/max/FindMaximum.java
+++ src/com/bridgelabz/max/FindMaximum.java
@@ -3,6 +3,7 @@ package com.bridgelabz.max;
 import java.util.Scanner;
 
 public class FindMaximum {
+
     public Integer maxNum(Integer input1, Integer input2, Integer input3){
         Integer max = input1;
         if(input2.compareTo(max) > 0)
@@ -12,9 +13,20 @@ public class FindMaximum {
         System.out.println("maximum of three integer is "+max);
         return max;
     }
+    
+    public Float maxNum(Float input1, Float input2, Float input3){
+        Float max = input1;
+        if(input2.compareTo(max) > 0)
+            max = input2;
+        if(input3.compareTo(max) > 0)
+            max = input3;
+        System.out.println("maximum of three float is "+max);
+        return max;
+    }
 
     public static void main(String[] args) {
         FindMaximum maximum=new FindMaximum();
         maximum.maxNum(2,4,6);
+        maximum.maxNum(3.5f,8.2f,2.1f);
     }
 }
