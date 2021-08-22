4ce7681d72f7e0157cbc73e2f9a7a02d733efe44
rojajullapalli <rojajules@gmail.com>
2021-08-22 17:17:15 +0530

    ADDED:Three String To Find Maximum

--- src/com/bridgelabz/max/FindMaximum.java
+++ src/com/bridgelabz/max/FindMaximum.java
@@ -24,9 +24,20 @@ public class FindMaximum {
         return max;
     }
 
+    public String maxNum(String input1, String input2, String input3){
+        String max = input1;
+        if(input2.compareTo(max) > 0)
+            max = input2;
+        if(input3.compareTo(max) > 0)
+            max = input3;
+        System.out.println("maximum of three float is "+max);
+        return max;
+    }
+
     public static void main(String[] args) {
         FindMaximum maximum=new FindMaximum();
         maximum.maxNum(2,4,6);
         maximum.maxNum(3.5f,8.2f,2.1f);
+        maximum.maxNum("apple", "pineapple" ,"orange");
     }
 }
