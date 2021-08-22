644c2f5e01fcfb4cf0d7242b4b9d9e8e0fcd13d1
rojajullapalli <rojajules@gmail.com>
2021-08-22 20:05:14 +0530

    ADDED:Max Method To take More Than Three Parameters

--- src/com/bridgelabz/max/FindMaximum.java
+++ src/com/bridgelabz/max/FindMaximum.java
@@ -1,41 +1,39 @@
 package com.bridgelabz.max;
 
-public class FindMaximum <T extends Comparable<T>> {
-    T input1, input2, input3;
+import java.util.ArrayList;
+import java.util.Collections;
 
-    public FindMaximum(T input1, T input2, T input3) {
-        this.input1 = input1;
-        this.input2 = input2;
-        this.input3 = input3;
-    }
-
-    public T testMaximum() {
-        return FindMaximum.getMax(input1,input2,input3);
-    }
-
-    public static <T extends Comparable<T>> T getMax(T input1, T input2, T input3){
-        T max=input1;
-        if(input2.compareTo(max)>0)
-            max=input2;
-        if(input3.compareTo(max)>0)
-            max=input3;
-        showMaxValue(input1, input2, input3, max);
-        return max;
-    }
-
-    private static <T extends Comparable<T>> void showMaxValue(T input1, T input2, T input3, T max) {
-        System.out.println("the input1 is "+input1+"\ninput2 is "+input2+"\ninput3 is "+input3);
-        System.out.println("Maximum : " + max+"\n");
-    }
+public class FindMaximum<T extends Comparable<T>>{
+        ArrayList<T> list = new ArrayList<T>();
 
 
-    public static void main(String[] args) {
-        Integer intValue1 = 300 , intValue2 = 900 , intValue3 = 60;
-        Float floatValue1 = 9.3f , floatValue2 = 3.5f , floatValue3 = 8.7f;
-        String stringValue1 = "Mango" , stringValue2 = "Orange" , stringValue3 = "PineApple";
-        new FindMaximum<>(intValue1, intValue2, intValue3).testMaximum();
-        new FindMaximum<>(floatValue1, floatValue2, floatValue3).testMaximum();
-        new FindMaximum<>(stringValue1, stringValue2, stringValue3).testMaximum();
+        //contructor accept multiple arguments
+        FindMaximum(T... inputs){
+            for (T value : inputs){
+                this.list.add(value);
+            }
+        }
 
-    }
+        public T testMaximum() {
+            return FindMaximum.testMaximum(list);
+        }
+
+        public static <T extends Comparable<T>> T testMaximum(ArrayList<T> list) {
+            // Sort the list in ascending order
+            Collections.sort(list);
+            // last position holds the max value
+            T maxValue = list.get(list.size() - 1);
+            System.out.println("Maximum : " + maxValue);
+            return maxValue;
+        }
+
+
+        public static void main(String[] args) {
+            Integer intValue1 = 300 , intValue2 = 900 , intValue3 = 60,intValue4=120;
+            Float floatValue1 = 9.3f , floatValue2 = 3.5f , floatValue3 = 8.7f, floatValue4=17.2f;
+            String stringValue1 = "Mango" , stringValue2 = "Orange" , stringValue3 = "PineApple", stringvalue4 = "Papaya";
+            new FindMaximum<>(intValue1, intValue2, intValue3, intValue4).testMaximum();
+            new FindMaximum<>(floatValue1, floatValue2, floatValue3, floatValue4).testMaximum();
+            new FindMaximum<>(stringValue1, stringValue2, stringValue3, stringvalue4).testMaximum();
+        }
 }
